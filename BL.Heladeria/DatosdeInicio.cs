d790a42dbbab78dff44b0a1e70ba623c978898ea
Asdertin <85328995+Asdertin@users.noreply.github.com>
2021-11-25 08:02:43 -0600

    Add files via upload

---
+++ BL.Heladeria/DatosdeInicio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Heladeria
+{
+    public class DatosdeInicio: CreateDatabaseIfNotExists<Contexto>
+    {
+        protected override void Seed(Contexto contexto)
+        {
+
+            var usuarioAdmin = new Usuario();
+            usuarioAdmin.Nombre = "matrix";
+            usuarioAdmin.Contrasena = "1234";
+
+            contexto.Usuarios.Add(usuarioAdmin);
+
+            var categoria1 = new Categoria();
+            categoria1.Descripcion = "MilkShake de Chocolate";
+            contexto.Categorias.Add(categoria1);
+
+            var categoria2 = new Categoria();
+            categoria2.Descripcion = "MilkShake de Fresa";
+            contexto.Categorias.Add(categoria2);
+
+            var categoria3 = new Categoria();
+            categoria3.Descripcion = "MilkShake de Vainilla";
+            contexto.Categorias.Add(categoria3);
+
+            var categoria4 = new Categoria();
+            categoria4.Descripcion = "Tooping Shake Fresa";
+            contexto.Categorias.Add(categoria4);
+
+            var tipo1 = new Tipo();
+            tipo1.Descripcion = "Conos";
+            contexto.Tipos.Add(tipo1);
+
+            var tipo2 = new Tipo();
+            tipo2.Descripcion = "Paletas";
+            contexto.Tipos.Add(tipo2);
+
+            var tipo3 = new Tipo();
+            tipo3.Descripcion = "Pasteles";
+            contexto.Tipos.Add(tipo3);
+
+            
+            base.Seed(contexto);
+        }
+    }
+}
